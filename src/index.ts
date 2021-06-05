d25539c4a291932393d654f15d958831c0f1cf12
akulsr0 <akulsr0@gmail.com>
2021-06-05 19:26:00 +0530

    fix: fixed icon size

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express, { Application } from "express";
-import cors from "cors";
 import ejs from "ejs";
 import path from "path";
 
 const app: Application = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../demos")));
